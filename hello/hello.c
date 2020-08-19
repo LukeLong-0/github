1830425cbb440ea2378417648d9a37d3664650c4
LukeLong-0 <69820090+LukeLong-0@users.noreply.github.com>
2020-08-19 14:27:31 -0500

    Initial commit

---
+++ hello/hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Luke Long
+ * Date: 8/18/20
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+     printf("Luke Long!\n");
+
+     return 0;
+ }
